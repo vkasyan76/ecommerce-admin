c491dbb9c6c00262b7a906a932a06630c70d8758
Valentyn Kasyan <vkasyan@gmx.de>
2023-08-08 15:50:12 +0200

    Settings page (Admin) - Part 1

---
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { Store } from "@prisma/client";
+import { Trash } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+
+interface SettingsFormProps {
+  initialData: Store;
+}
+
+export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading title="Settings" description="Manage store preferences" />
+        <Button variant="destructive" size="icon" onClick={() => {}}>
+          <Trash className="h-4 w-4" />
+        </Button>
+      </div>
+      <Separator />
+    </>
+  );
+};
