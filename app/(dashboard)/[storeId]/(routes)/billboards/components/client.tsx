14c8e0065b2ccbc7d955d3ebf7a24ecd761a51e5
Valentyn Kasyan <vkasyan@gmx.de>
2023-08-17 12:47:41 +0200

    Data Table (Admin) - Part 4: API List in Billboard Tab & Clerk Middleware update

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -7,6 +7,7 @@ import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { DataTable } from "@/components/ui/data-table";
 import { BillboardColumn, columns } from "./columns";
+import { ApiList } from "@/components/ui/api-list";
 
 interface BillboardClientProps {
   // data: Billboard[];
@@ -33,6 +34,9 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
       </div>
       <Separator />
       <DataTable searchKey="label" columns={columns} data={data} />
+      <Heading title="API" description="API calls for Billboards" />
+      <Separator />
+      <ApiList entityName="billboards" entityIdName="billboardId" />
     </>
   );
 };
